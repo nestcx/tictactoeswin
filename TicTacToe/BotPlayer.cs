19fec65acfe95549fa39727c60de8c3f8fb0ac00
Marcus <marcusvalentino2013@gmail.com>
2017-08-16 15:13:18 +1000

    add and implement cp ai

---
+++ TicTacToe/BotPlayer.cs
@@ -0,0 +1,17 @@
+﻿using System;
+namespace tictactoe
+{
+	public class BotPlayer
+	{
+		public BotPlayer()
+		{
+		}
+
+		public static int get_move(string[] cells)
+		{
+			// return -1; causes broken loop!
+			Random rng = new Random(); // Should this be created each time? Hmm...
+			return rng.Next(9); // From 0 up to (but not including) 9
+		}
+	}
+}
